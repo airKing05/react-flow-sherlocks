6a64e6b8e62530e321c3a601bea1d4f5bd7648b6
airKing05 <anil050797@gmail.com>
2025-12-13 20:28:58 +0530

    diable the buttons of nav, when reach to the end

--- src/ui/NodeNav.jsx
+++ src/ui/NodeNav.jsx
@@ -1,28 +1,57 @@
-import React, { useState } from "react";
+import React from "react";
 import "./NodeNav.css";
 
-const NodeNav = ({ isActive, current, total, onNext, onPrev, onClose, selectedNode }) => {
+const NodeNav = ({
+  isActive,
+  current,
+  total,
+  onNext,
+  onPrev,
+  onClose,
+  selectedNode,
+}) => {
   if (!isActive) return null;
 
-  console.log("selectedNode", selectedNode)
+  const isPrevDisabled = current <= 1;
+  const isNextDisabled = current >= total;
+
+
+  // if (current === 1) return;
+  // if (current === total) return;
+
 
   return (
     <div className="capsule">
       <div className="container">
-        <button className="icon-btn" onClick={onPrev}>&#10094;</button>
+        <button
+          className="icon-btn"
+          onClick={onPrev}
+          disabled={isPrevDisabled}
+        >
+          &#10094;
+        </button>
 
         <div className="middle">
-            <div className="badge">{current}/{total}</div>
-            <div className="title">{selectedNode?.data?.label}</div>
+          <div className="badge">
+            {current}/{total}
+          </div>
+          <div className="title">{selectedNode?.data?.label}</div>
         </div>
 
-        <button className="icon-btn" onClick={onNext}>&#10095;</button>
+        <button
+          className="icon-btn"
+          onClick={onNext}
+          disabled={isNextDisabled}
+        >
+          &#10095;
+        </button>
       </div>
 
-      <button className="icon-btn close" onClick={onClose}>&#10005;</button>
+      <button className="icon-btn close" onClick={onClose}>
+        &#10005;
+      </button>
     </div>
   );
 };
-
 
 export default NodeNav;
